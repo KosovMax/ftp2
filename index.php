62cf26192697aa129320d5b7bcc436ad67e01b41
KosovMax <kosovmax@hotmail.com>
2016-02-14 22:37:39 +0200

    update task

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once 'app/start.php';
 
 $ftp = new Ftp("chc-deaf.org.ua");
 $ftp->login("chc-deaf", "moskvich408");
+// Парамети: 1-Завантажувати на ФС; 2-Взяти з локальна файл; 3-Формати файл;
 $ftp->push_file("/file/maxim3.jpg", "maxim3.jpg", "gif,png,jpg");
 //$ftp->push_file("C:\ftp\file\maxim3.jpg", "maxim3.jpg", "gif,png,jpg"); // For Windows
 $ftp->close();
